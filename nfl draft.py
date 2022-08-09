b1ee86021e49fddb4571f3a7b0d18ea2c501cf52
Eric Born <eric.born85@gmail.com>
2022-08-09 10:55:18 -0500

    Update nfl draft.py

--- nfl draft.py
+++ nfl draft.py
@@ -11,6 +11,8 @@ import pandas as pd
 file_path = r'C:/Users/Eric/Documents/GitHub/nfl_draft/data/'
 
 # import stat files
+# TODO 
+# need to check data for only regular season games
 pass_rush_df = pd.read_csv(file_path + 'pass_rush.csv')
 defense_df = pd.read_csv(file_path + 'defense.csv')
 kicking_df = pd.read_csv(file_path + 'kicking.csv')
@@ -52,7 +54,7 @@ offensive_score_list[0].name = 'player'
 offensive_score_list[1].name = 'pos'
 offensive_score_list[2].name = 'game_id'
 offensive_score_list[3].name = 'pass_yds'
-offensive_score_list[4].name = 'pass_rd'
+offensive_score_list[4].name = 'pass_td'
 offensive_score_list[5].name = 'pass_int'
 offensive_score_list[6].name = 'rush_yds'
 offensive_score_list[7].name = 'rush_td'
@@ -72,13 +74,45 @@ score_df = pd.concat([offensive_score_list[0],offensive_score_list[1],
            offensive_score_list[10],offensive_score_list[11],
            offensive_score_list[12],offensive_score_list[13]], axis=1)
 
-# calculate avg points scored per game, total season points
-for name in score_df['player'].unique():
-    print(name)
-    score_df['pass_yds'].mean(score_df.loc[score_df['player'] == name])
+# calculate avg points scored per game and total season points
+# need to add adjusted average based on games played
+# should track if injured or other cause of games missed
+calc_score_list = [score_df.groupby('player').pass_yds.mean(),
+                   score_df.groupby('player').pass_yds.sum(),
+                   score_df.groupby('player').pass_td.mean(),
+                   score_df.groupby('player').pass_td.sum(),
+                   score_df.groupby('player').pass_int.mean(),
+                   score_df.groupby('player').pass_int.sum(),
+                   score_df.groupby('player').rush_yds.mean(),
+                   score_df.groupby('player').rush_yds.sum(),
+                   score_df.groupby('player').rush_td.mean(),
+                   score_df.groupby('player').rush_td.sum(),
+                   score_df.groupby('player').rec.mean(),
+                   score_df.groupby('player').rec.sum(),
+                   score_df.groupby('player').rec_yds.mean(),
+                   score_df.groupby('player').rec_yds.sum(),
+                   score_df.groupby('player').rec_td.mean(),
+                   score_df.groupby('player').rec_td.sum(),
+                   score_df.groupby('player').two_point_conv.mean(),
+                   score_df.groupby('player').two_point_conv.sum(),
+                   score_df.groupby('player').fumbles_lost.mean(),
+                   score_df.groupby('player').fumbles_lost.sum(),
+                   score_df.groupby('player').offensive_fumble_recovery_td.mean(),
+                   score_df.groupby('player').offensive_fumble_recovery_td.sum()]
 
+# convert series data to dataframe with index as the players name
+calc_score_df = pd.concat([calc_score_list[0],calc_score_list[1],
+                            calc_score_list[2],calc_score_list[3],
+                            calc_score_list[4],calc_score_list[5],
+                            calc_score_list[6],calc_score_list[7],
+                            calc_score_list[8],calc_score_list[9],
+                            calc_score_list[10],calc_score_list[11],
+                            calc_score_list[12],calc_score_list[13],
+                            calc_score_list[14],calc_score_list[15],
+                            calc_score_list[16],calc_score_list[17],
+                            calc_score_list[18],calc_score_list[19],
+                            calc_score_list[20],calc_score_list[21]], axis = 1)
 
-# filter data down to regular season games
 
 # evaluate players point generation per position
 # evaluate teams point generation per position
